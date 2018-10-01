2536ac55cc89707b98dfa113609e9ccb5ac4e95d
scorsi <sylvain.corsini@gmail.com>
2018-10-01 15:13:38 +0200

    Add apollo graphql

--- src/server.js
+++ src/server.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import {ApolloServer, gql} from "apollo-server-express";
 
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+const resolvers = {
+  Query: {
+    hello: () => "world"
+  }
+};
 
 const app = express();
+
+const apollo = new ApolloServer({
+  typeDefs,
+  resolvers,
+  path: "/graphql"
+});
+
+apollo.applyMiddleware({ app });
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
