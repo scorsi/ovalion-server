4258674b12c2e017c39cffa00305cf00f4588a02
scorsi <sylvain.corsini@gmail.com>
2018-11-08 19:01:59 +0100

    Update and add new features to users

--- src/user.js
+++ src/user.js
@@ -1,9 +1,10 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-import client from './databaseclient';
+import client from "./databaseclient";
 
-const cert = 'shhhhhh';
+
+const cert = "shhhhhh";
 
 const createHash = async (data) => {
   return await bcrypt.hash(data, 20);
@@ -13,51 +14,70 @@ const compareHash = async (d1, d2) => {
   return await bcrypt.compare(d1, d2);
 };
 
-const getAllUser = async (root, {}, context) => {
-  const {rows: users} = await client.query('SELECT * FROM users;');
+export const getAllUsers = async (actualUser) => {
+  const {rows: users} = await client.query(
+    "SELECT * FROM users;");
+
   return users.map((user) => ({
     ...user,
-    createdAt: Math.floor(user.createdAt / 1000)
+    createdAt: Math.floor(user.created_at / 1000)
   }));
 };
 
-const getUserByUsername = async (root, {username}, context) => {
-  const res = await client.query('SELECT * FROM users WHERE username = $1', [username]);
-  const user = res.rows[0];
-  if (user === undefined) return null;
-  return {
+export const getUserByUsername = async (actualUser, username) => {
+  const {rows: users} = await client.query(
+    "SELECT * FROM users WHERE username = $1 LIMIT 1;",
+    [username]);
+
+  const user = users[0];
+  if (user !== undefined) return {
     ...user,
-    createdAt: Math.floor(user.createdAt / 1000)
+    createdAt: Math.floor(user.created_at / 1000)
   };
+  else return null;
 };
 
-const createUser = async (root, {username, email, password}, context) => {
-  const hasUser = (await client.query('SELECT 1 FROM users WHERE username = $1 OR email = $2 LIMIT 1;', [username, email])).rows.length !== 0;
-  if (!hasUser) {
-    await client.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)', [username, email, createHash(password)]);
+export const getUserById = async (actualUser, userId) => {
+  const {rows: users} = await client.query(
+    "SELECT * FROM users WHERE id = $1 LIMIT 1;",
+    [userId]);
+
+  const user = users[0];
+  if (user !== undefined) return {
+    ...user,
+    createdAt: Math.floor(user.created_at / 1000)
+  };
+  else return null;
+};
+
+export const createUser = async (actualUser, username, email, password) => {
+  const {rows: users} = await client.query(
+    "SELECT 1 FROM users WHERE username = $1 OR email = $2 LIMIT 1;",
+    [username, email]);
+
+  if (users.length === 0) {
+    await client.query(
+      "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
+      [username, email, createHash(password)]);
     return getUserByUsername(root, {username}, context);
-  } else {
-    return null;
-  }
+  } else return null;
 };
 
-const authenticateUser = async (root, {username, password}, context) => {
-  const res = await client.query('SELECT id, username, email, password, "createdAt" FROM users WHERE username = $1 LIMIT 1;', [username]);
-  if (res.rows.length === 0) return null;
-  const user = res.rows[0];
-  if (compareHash(user.password, password)) {
-    return jwt.sign(user, cert);
-  } else {
-    return null;
-  }
+export const authenticateUser = async (actualUser, username, password) => {
+  const {rows: users} = await client.query(
+    "SELECT * FROM users WHERE username = $1 LIMIT 1;",
+    [username]);
+
+  const user = users[0];
+  if (user === undefined) return null;
+  if (compareHash(user.password, password)) return {token: jwt.sign(user, cert), user};
+  else return null;
 };
 
-const validateUser = (token) => {
+export const validateUser = (token) => {
   try {
     return jwt.verify(token, cert);
   } catch (e) {
     return null;
   }
 };
-
-export {getAllUser, getUserByUsername, createUser, authenticateUser, validateUser};
